f5cf09f1fbc913161cbb7a98cb3285ab670592ae
Mathias Petak <wi18b085@technikum-wien.at>
2021-01-21 15:15:17 +0100

    Projektdateien hinzufügen.

---
+++ Zahlenkonverter/SpecFlowFeature1.feature
@@ -0,0 +1,10 @@
+﻿Feature: SpecFlowFeature1
+	In order to convert numbers
+	As a math idiot
+	I want to be told the coverted numbers of hexadecimal, octal and binary 
+
+@mytag
+Scenario: hexadecimal conversion
+	Given the number is "affe"
+	When the conversion is hexadecimal
+	Then the result should be 45054
---
+++ Zahlenkonverter/SpecFlowFeature1Steps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Zahlenkonverter
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private string number;
+        private int result;
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(string p0)
+        {
+            number = "affe";
+        }
+        
+        [When(@"the conversion is hexadecimal")]
+        public void WhenTheConversionIsHexadecimal()
+        {
+            result = System.Convert.ToInt32(number, 16);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
